c35c7cba5f9b3bb706aaa5d1059ffd68d8fc29ba
mateuszGrabiec <mateuszgrabiec7@gmail.com>
2020-03-26 21:38:06 +0100

    scoring #SR-33

--- Assets/Scripts/PlatformGeneretor.cs
+++ Assets/Scripts/PlatformGeneretor.cs
@@ -12,17 +12,17 @@ public class PlatformGeneretor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        thePlatform = GameObject.Find("Platform");
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < generationPoint.position.x)
+        if (transform.position.x < generationPoint.position.x)
         {
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBeetwen, transform.position.y,0);
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBeetwen, transform.position.y, 0);
             Instantiate(thePlatform, transform.position, transform.rotation);
-
         }
     }
 }
---
+++ Assets/Scripts/ScoreMenager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreMenager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highScoreText;
+
+    public double scoreCounter;
+    public double highScoreCounter;
+
+    public float PointPerSecond;
+
+    public bool isScoring;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isScoring)
+        {
+            float scoreTemp = PointPerSecond * Time.deltaTime;
+            scoreTemp = Mathf.Floor(scoreTemp);
+            scoreCounter += scoreTemp;
+            if(highScoreCounter<scoreCounter) highScoreCounter = scoreCounter;
+
+
+            scoreText.text = "Score: " + scoreCounter.ToString();
+            highScoreText.text = "High Score: " + highScoreCounter;
+        }
+    }
+}
