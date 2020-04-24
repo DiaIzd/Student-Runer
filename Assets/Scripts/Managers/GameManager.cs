1595efcb69ae32d8486b31b62297d05ec3ff6928
mateuszGrabiec <mateuszgrabiec7@gmail.com>
2020-04-24 11:07:23 +0200

    -fix repo

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+
+    public ScoreMenager theScoreManager;
+    public Student thePlayer;
+    public DeathMenu theDeathMenu;
+
+
+    private void Start()
+    {
+        theScoreManager = FindObjectOfType<ScoreMenager>();
+    }
+
+
+    public void GameOver()
+    {
+        if (gameHasEnded == false)
+        {
+            theScoreManager.isScoring = false;
+            setHighScore(theScoreManager.scoreCounter);
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            
+            thePlayer.gameObject.SetActive(false);
+            theDeathMenu.gameObject.SetActive(true);
+        }
+        
+    }
+
+    private bool setHighScore(float scoreCounter)
+    {
+        if (PlayerPrefs.GetFloat("HighScore") < scoreCounter)
+        {
+            PlayerPrefs.SetFloat("HighScore3", PlayerPrefs.GetFloat("HighScore2"));
+            PlayerPrefs.SetFloat("HighScore2", PlayerPrefs.GetFloat("HighScore"));
+            PlayerPrefs.SetFloat("HighScore", scoreCounter);
+            return true;
+        }
+        else if (PlayerPrefs.GetFloat("HighScore2") < scoreCounter)
+        {
+            PlayerPrefs.SetFloat("HighScore3", PlayerPrefs.GetFloat("HighScore2"));
+            PlayerPrefs.SetFloat("HighScore2", scoreCounter);
+            return true;
+        }
+        else if (PlayerPrefs.GetFloat("HighScore3") < scoreCounter)
+        {
+            PlayerPrefs.SetFloat("HighScore3", scoreCounter);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Platforms/PlatformGeneretor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGeneretor : MonoBehaviour
+{
+    public Transform generationPoint;
+
+    public float minDistanceBeetween;
+    public float maxDistanceBeetween;
+
+    public ObjectPooler objectPooler;
+    public float buffPropability;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < generationPoint.position.x)
+        {
+            float distanceBeetwen = Random.Range(minDistanceBeetween,maxDistanceBeetween);
+            GameObject newPlatform=objectPooler.GetPolledPlatform();
+            float height= Random.Range(-0.15f, 0.25f);
+            transform.position = new Vector3(transform.position.x + newPlatform.GetComponent<BoxCollider2D>().size.x + distanceBeetwen, height, 0);
+            newPlatform.tag = "platform";
+            newPlatform.SetActive(true);
+            Instantiate(newPlatform, transform.position, transform.rotation);
+
+            //buff
+            if (Random.Range(0, 100) < buffPropability)
+            {
+                GameObject newBuff = objectPooler.GetPolledBuff();
+                transform.position = new Vector3(transform.position.x, height + 0.2f, 0);
+                newBuff.SetActive(true);
+                Instantiate(newBuff, transform.position, transform.rotation);
+            }
+        }
+    }
+}
